46e2c28c2f9c3333353acd038f82cba483e7a227
Eche <echezona2000@gmail.com>
2026-09-20 19:04:05 -0400

    DSL initial commit

---
+++ src/query2.py
@@ -0,0 +1,15 @@
+from src.list_load import list_data_collect
+
+import pandas as pd
+import numpy as np
+
+
+def query2():
+    churn_list = list_data_collect()
+
+    query2_result = churn_list[
+        (churn_list['InternetService'] == 'DSL') &
+        (churn_list['tenure'] < 20)
+   ][['customerID', 'gender', 'InternetService', 'tenure']]
+
+    return query2_result
